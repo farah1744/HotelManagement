eb40629ab074c342a133e2dd6992bc0a63ba0772
Farah Matallaoui <matallaouifarah17@gmail.com>
2023-01-23 17:43:41 +0100

    Add project files.

---
+++ WebApplication9/Controllers/RoomController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication9.Models;
+using WebApplication9.ViewModel;
+
+namespace WebApplication9.Controllers
+{
+    public class RoomController : Controller
+
+    {
+        private WPFEntities1 objWPFEntities;
+        public RoomController()
+        {
+            objWPFEntities = new WPFEntities1();
+
+        }
+        // GET: Room
+        public ActionResult Index()
+        {
+            RoomViewModel objRoomViewModel = new RoomViewModel();
+
+            objRoomViewModel.ListOfBookingStatus = (from obj in objWPFEntities.BookingStatus
+                                                    select new SelectListItem()
+                                                    {
+                                                        Text = obj.BookingStatus,
+                                                        Value = obj.BookingStatusId.ToString()
+                                                    }).ToList();
+
+            objRoomViewModel.ListOfRoomType = (from obj in objWPFEntities.TypeChambreTs
+                                                    select new SelectListItem()
+                                                    {
+                                                        Text = obj.typeChambre,
+                                                        Value = obj.idTypeChambre.ToString()
+                                                    }).ToList();
+            return View(objRoomViewModel);
+        }
+    }
+}
---
+++ WebApplication9/ViewModel/RoomViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication9.ViewModel
+{
+    public class RoomViewModel
+    {
+        public int IdChambre { get; set; }
+        public string NumChambre { get; set; }
+
+        public string ImageChambre { get; set; }
+        public decimal PrixChambre { get; set; }
+        public int BookingStatusId { get; set; }
+        public int idTypeChambre { get; set; }
+        public int CapaciteChambre { get; set; }
+        public HttpPostedFileBase Image { get; set; }
+        public string DescriptionChambre { get; set; }
+        public List<SelectListItem> ListOfBookingStatus { get; set; }
+        public List<SelectListItem> ListOfRoomType { get; set; }
+
+    }
+}
